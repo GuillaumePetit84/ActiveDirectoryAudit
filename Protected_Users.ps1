b807aa4ac05d18fb3fb91945eae4b6de4ae29d0a
123soleil <66068507+GuillaumePetit84@users.noreply.github.com>
2021-04-26 15:45:41 +0200

    Add files via upload

---
+++ Protected_Users.ps1
@@ -0,0 +1,31 @@
+# DATA :
+$USERS = Get-ADGroupMember -Identity "Protected Users" -Recursive | Select SamAccountName
+$UAC = $USERS | ForEach-Object { Get-ADUser $_.SamAccountName -Properties userAccountControl} | Select SamAccountName,userAccountControl
+
+# TRANSFORM user :
+$TOTAL_LENGTH = 32
+
+$UAC.PSObject.Properties | ForEach-Object {
+    foreach ($value in $_.Value)
+    {
+        if ($value.SamAccountName)
+        {
+            $UAC_BINARY = [Convert]::ToString($value.userAccountControl,2)
+            $OFFSET = $TOTAL_LENGTH - $UAC_BINARY.Length
+            $UAC_FULL = "0" * $OFFSET
+            $UAC_FINAL = "$UAC_FULL$UAC_BINARY"
+
+            Write-Host $value.SamAccountName
+
+            if ($UAC_FINAL[11] -eq "1")
+            {
+                Write-Host -ForegroundColor green "[+] L'authentification du compte ne peux pas être délégué"
+            }
+            else
+            {
+                Write-Host -ForegroundColor red "[!] L'authentification du compte peux être délégué"
+            }
+            Write-Host ""
+        }
+    }
+}
